973652f7f52d02f416a8084ed8e9ca7d1b706e88
KevinZYC <kevinzyc98@gmail.com>
2022-11-06 11:34:47 -0500

    xaml outline for student

---
+++ SoundsEasyV1/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundsEasyV1
+{
+    internal class Student
+    {
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string course { get; set; }
+        public int grade { get; set; }
+        public string email { get; set; }
+
+
+        public Student(string fname, string lname, string course, int grade, string email)
+        {
+            this.fname = fname;
+            this.lname = lname;
+            this.course = course;
+            this.grade = grade;
+            this.email = email;
+        }
+    }
+}
